3f0f92fcdc89951d669ed99b6272443d2cc22929
Jeremy-VALADE <jerev@LAPTOP-OCSONCR7>
2019-12-20 18:29:05 +0100

    Fin partie2.2

--- application/views/templates/menuAdmin.php
+++ application/views/templates/menuAdmin.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="<?php echo site_url('Adherent/accueil'); ?>">Accueil</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler btn-lg" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
