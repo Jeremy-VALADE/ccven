1888b2629264b4cf992ceba3f224c9e950a24585
Jeremy-VALADE <jerev@LAPTOP-OCSONCR7>
2019-12-22 18:29:19 +0100

    Juste à commenter le code .2

--- application/views/adherent/accueil.php
+++ application/views/adherent/accueil.php
@@ -1,3 +1,3 @@
-<h2>Bienvenue au Cvven</h2>
+<h2>Bienvenue au Cven</h2>
 
 <img class="imgAccueil" style="display: block; margin-right: auto; margin-left: auto;" src="<?php echo base_url(); ?>images/presentation.jpg">
